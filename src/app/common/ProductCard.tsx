859418d9188daea63acbfaaf232a4a30a218298c
24HP8411 24HP8411 <24HP8411@admins-iMac.local>
2025-03-17 17:59:05 +0800

    feat(products): sample route

--- src/app/common/ProductCard.tsx
+++ src/app/common/ProductCard.tsx
@@ -1,26 +1,37 @@
-export default function ProductCard() {
-    return (
-      <div className="bg-white shadow-lg rounded-lg overflow-hidden w-64">
-        <div className="relative">
-          <img 
-            src="https://placehold.co/300x600" 
-            alt="Product" 
-            className="w-full h-40 object-cover"
-          />
-          <span className="absolute top-2 left-2 bg-blue-500 text-white text-xs font-semibold px-2 py-1 rounded">
-            Category
-          </span>
-        </div>
-  
-        <div className="p-4">
-          <h3 className="text-lg font-semibold text-gray-800">Product Name</h3>
-          <p className="text-gray-600 text-sm mt-1">$99.99</p>
-  
-          <button className="mt-3 w-full bg-[#3e15e2ef] text-white py-2 rounded hover:bg-[#3a3992]">
-            Add to Cart
-          </button>
-        </div>
+import { Product } from "@/types/product";
+import Link from "next/link";
+import Image from "next/image";
+
+type ProductCardProps = {
+  product: Product;
+};
+
+export const ProductCard = ({ product }: ProductCardProps) => {
+  return (
+    <div className="bg-white shadow-lg rounded-lg overflow-hidden w-64">
+      <div className="relative">
+        <Image 
+          width={400}
+          height={250}
+          alt={product.name}
+          src={product.image}
+          className="w-full h-40 object-cover"
+        />
+        <span className="absolute top-2 left-2 bg-blue-500 text-white text-xs font-semibold px-2 py-1 rounded">
+          {product.tag}
+        </span>
       </div>
-    );
-  }
-  
+
+      <div className="p-4">
+        <h3 className="text-lg font-semibold text-gray-800">{product.name}</h3>
+        <p className="text-gray-600 text-sm mt-1">${product.price}</p>
+
+        <Link href={`/product/${product.id}`}>
+          <button className="mt-3 w-full bg-[#3e15e2ef] text-white py-2 rounded hover:bg-[#3a3992]">
+            View Details
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
---
+++ src/types/product.ts
@@ -0,0 +1,15 @@
+export interface Product {
+  id: string;
+  name: string;
+  tag: string;
+  description: string;
+  price: string;
+  image: string;
+};
+
+export type Comment = {
+  comment: string;
+  text: string;
+  date: string;
+};
+
